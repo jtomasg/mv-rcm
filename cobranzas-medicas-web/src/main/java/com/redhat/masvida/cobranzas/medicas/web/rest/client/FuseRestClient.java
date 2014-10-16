437694fce572c318230e14e026236c0473575874
jtomasg <jtomasg@gmail.com>
2014-10-16 09:37:26 -0300

    Cambios respecto a llamadas a capa de servicios mediante Rest

--- cobranzas-medicas-web/src/main/java/com/redhat/masvida/cobranzas/medicas/web/rest/client/FuseRestClient.java
+++ cobranzas-medicas-web/src/main/java/com/redhat/masvida/cobranzas/medicas/web/rest/client/FuseRestClient.java
@@ -2,8 +2,8 @@ package com.redhat.masvida.cobranzas.medicas.web.rest.client;
 
 import java.util.List;
 
-import javax.annotation.ManagedBean;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
@@ -18,6 +18,7 @@ import com.redhat.masvida.cobranzas.medicas.common.vo.business.PagoVO;
 import com.redhat.masvida.cobranzas.medicas.common.vo.business.RcmVO;
 import com.redhat.masvida.cobranzas.medicas.common.vo.business.RecepcionCobranzaMedicaVO;
 import com.redhat.masvida.cobranzas.medicas.common.vo.business.TipoPagoVO;
+import com.redhat.masvida.cobranzas.medicas.web.util.RestClientCallUtil;
 
 @ManagedBean
 @RequestScoped
@@ -27,6 +28,9 @@ public class FuseRestClient {
 
 	@Inject
 	private FuseRestDummyData db;
+	
+	@Inject
+	private RestClientCallUtil restClientCallUtil;
 
 	public String getNombreAgencia(Integer id)
 			throws AgenciaNoEncontradaException {
@@ -75,6 +79,24 @@ public class FuseRestClient {
 	}
 
 	public RcmVO findFolio(Integer folio) {
+		
+		try{
+			
+			String endpointURL = "http://localhost:8080/miRest/rcm/buscar/"+folio+"&01-01-2014&31-12-2015";
+			String json = restClientCallUtil.callJsonRemoteRest(endpointURL);
+			
+			System.out.println("JSON 1: "+json);
+			
+//			ObjectMapper mapper = new ObjectMapper();
+//			RcmVO rcmVO = mapper.readValue(json, RcmVO.class);
+			
+			endpointURL = "http://localhost:8080/miRest/rcm/buscar/"+folio+"&01-01-2014&31-12-2015";
+			json = restClientCallUtil.callJsonRemoteRest(endpointURL, new RcmVO());
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
 		return db.findFolio(folio);
 	}
 
